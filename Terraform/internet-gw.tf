aba953edfe88c2b460f0cfb5ccf0b8d1e73e1be9
sakshishettyy08 <sakshi.shetty@techconsulting.tech>
2025-09-11 11:32:26 -0400

    removing files

--- Terraform/internet-gw.tf
+++
@@ -1,8 +0,0 @@
-# Creating Internet Gateway
-resource "aws_internet_gateway" "bankapp-igw" {
-  vpc_id = aws_vpc.bankapp-vpc.id
-
-  tags = {
-    Name = var.bankapp-igw
-  }
-}
--- Terraform/key-pair.tf
+++
@@ -1,6 +0,0 @@
-# Creating Key Pair
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "bankapp-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsJe6O4+s5nqKXz5oDfzUzf/gX4DIVcWKEb4fSdNNHr madhup@DESKTOP-FR8TKPV"
-}
